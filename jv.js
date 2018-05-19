bcc367f497bee8d00d4e3f65f96e35689d3a7654
architasajjan <38557942+architasajjan@users.noreply.github.com>
2018-05-19 17:58:32 +0530

    Create jv.js

---
+++ jv.js
@@ -0,0 +1,6 @@
+function my(){
+document.getElementById("demo").innerHTML="SMPs:"<br>+ <ol><li>web-app(IEEE)</li>
+<li>polymer materials(IEEE)</li>
+<li>stock market analysis(ISTE)</li>
+<li>marketing(ISTE)</li>
+<li>macroeconomics(ACM)</li></ol> }
