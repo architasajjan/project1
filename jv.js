9800a7e31e8ad1eb6d478d6f10cf07d5a1f96fdc
architasajjan <38557942+architasajjan@users.noreply.github.com>
2018-05-19 19:14:19 +0530

    Delete jv.js

--- jv.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-document.getElementById("demo").innerHTML="SMPs:"<br>+ <ol><li>web-app(IEEE)</li>
-<li>polymer materials(IEEE)</li>
-<li>stock market analysis(ISTE)</li>
-<li>marketing(ISTE)</li>
-<li>macroeconomics(ACM)</li></ol> }
